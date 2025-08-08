f721ec249eabf56dc59455715786039451efe277
Dylan Hartnett <dylan.hartnett@hotmail.com>
2025-08-07 21:51:57 -0700

    Update and rename IRdeocder.ino to Main code file.ino
    Main code file. Uses an external IR Receiver library

--- Main code file.ino
+++ Main code file.ino
@@ -2,10 +2,10 @@
 #include <LiquidCrystal.h>
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
-#define IR_RECEIVE_PIN A0  // Change this if your IR receiver is on another pin
+#define IR_RECEIVE_PIN A0 //analog pin to recieve data
 int ledpin = 13;
 void setup() {
-  Serial.begin(115200); // Start serial at 115200 
+  Serial.begin(115200); 
   IrReceiver.begin(IR_RECEIVE_PIN, ENABLE_LED_FEEDBACK); // Start the IR receiver
   Serial.println("IR Receiver Ready - Press a button on your remote");
   lcd.begin(16, 2); //initializes display size to show that many columns and rows. Must be called in setup and manage display memory
@@ -14,9 +14,9 @@ void setup() {
 
 void loop() {
   delay(100);
-  lcd.setCursor(0, 0);         // First column, second line
+  lcd.setCursor(0, 0);         // First column, first line
   if (IrReceiver.decode()) {
-  digitalWrite(ledpin, HIGH);
+  digitalWrite(ledpin, HIGH); //LED turns on when signal received
     delay(700);
     // Print raw data in old HEX format
     Serial.print("Raw Data (HEX): 0x");
@@ -29,9 +29,9 @@ void loop() {
     //IrReceiver.printIRSendUsage(&Serial);
 
     IrReceiver.resume(); // Ready for next IR signal
-    lcd.print("HEX: 0x");
+    lcd.print("HEX: 0x");  //print HEX on the LCD screen
     lcd.print((unsigned long)IrReceiver.decodedIRData.decodedRawData, HEX);
   }
-  digitalWrite(ledpin, LOW);
+  digitalWrite(ledpin, LOW); //turns off LED after HEX printed
   
 }
